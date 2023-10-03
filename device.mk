47a62e79fca334e1a7782ac70995e87144b0869a
tu4wkl <pachadomenic2010@gmail.com>
2023-10-03 18:34:36 +0300

    Source release

---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a02
+
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
